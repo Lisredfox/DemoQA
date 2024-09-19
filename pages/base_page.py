23273f13e720d65ad14b06bf2cb03cf99a94780c
Lisrdfox <marinagaleevaorg@gmail.com>
2024-09-19 12:03:39 +0300

    Commit again

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,9 +3,9 @@ import time
 
 class BasePage:
 
-    def __init__(self, driver):
+    def __init__(self, driver, base_url):
         self.driver = driver
-        self.base_url = 'https://demoqa.com/'
+        self.base_url = base_url
 
     def visit(self):
         return self.driver.get(self.base_url)
@@ -16,3 +16,9 @@ class BasePage:
 
     def get_url(self):
         return self.driver.current_url
+
+    def equal_url(self):
+        if self.get_url() == self.base_url:
+            return True
+        else:
+            return False
